77109074599779429ee27dcb4d7a6194dbd606ca
lip <3192997488@qq.com>
2023-07-05 22:28:38 +0800

    更新.gitignore，删除build

--- build/CMakeFiles/richman_2023.dir/src/Player.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/richman_2023.dir/src/Player.c.o: \
- /home/lighthouse/richman_2023/src/Player.c /usr/include/stdc-predef.h \
- /home/lighthouse/richman_2023/./include/Player.h \
- /home/lighthouse/richman_2023/./include/global.h
