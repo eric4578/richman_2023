565367dade2fedaa5bcc82980a05edde2350dcf6
lip <3192997488@qq.com>
2023-07-05 22:32:38 +0800

    mapNode的user长度加一

--- include/map.h
+++ include/map.h
@@ -9,7 +9,7 @@ typedef struct mapNode{/*whom->whose;user->player;*/
     int whose; // '0'为无人空地，1234对应不同玩家
     int level; // '0'-'3'级
     int item[MAX_TOOL_NUM]; // bool 量，0弃用，存在某一个道具，2为机器娃娃弃用
-    char user[MAX_PLAYER_NUM]; // 在此地的玩家,初始为0,user[0]是最后到的玩家
+    char user[MAX_PLAYER_NUM + 1]; // 在此地的玩家,初始为0,user[0]是最后到的玩家
     char ch;// 显示字符
     int color;
 }mapnode;
