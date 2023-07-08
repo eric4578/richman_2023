dc6dc9819e4bf71a2e9c5d18c8957c53fea7823f
dadaguai12315 <lxinyi20210419@163.com>
2023-07-08 11:04:47 +0800

    merge:lxy

--- build/CMakeFiles/richman.dir/src/Player.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/richman.dir/src/Player.c.o: /home/xy/richman_2023/src/Player.c \
- /usr/include/stdc-predef.h /home/xy/richman_2023/./include/Player.h \
- /home/xy/richman_2023/./include/global.h
