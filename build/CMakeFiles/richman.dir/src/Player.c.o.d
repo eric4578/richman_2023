8200cafb98ef619a58b7e7bf7f2c1f771fb08458
JiangYilun <nnyilun@126.com>
2023-07-08 09:30:00 +0800

    0708

--- build/CMakeFiles/richman.dir/src/Player.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/richman.dir/src/Player.c.o: \
- /home/lighthouse/dir/richman_2023/src/Player.c \
- /usr/include/stdc-predef.h \
- /home/lighthouse/dir/richman_2023/./include/Player.h \
- /home/lighthouse/dir/richman_2023/./include/global.h
